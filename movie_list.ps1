45c19d55c0c259c2b47f1a81131b7dfcef608034
Morris Tai <morristai01@gmail.com>
2020-04-29 01:24:25 +0800

    add file regex match function

--- movie_list.ps1
+++ movie_list.ps1
@@ -1,1 +1,4 @@
 echo "Starting Mailing Task"
+cd C:\Users\shanglieh\Documents\movie_list
+tree 'D:\電影' /F > movie_list.txt
+echo "Done!"
--- send_email.go
+++ send_email.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"crypto/tls"
-
+	"fmt"
 	"gopkg.in/mail.v2"
+	"os"
+    "path/filepath"
 )
 
 func main() {
+	files, err := WalkMatch("/home/morris/Videos", "*.txt")
+    if err != nil{
+		fmt.Println(err)
+	}
 	m := mail.NewMessage()
 	m.SetHeader("From", "diaper151@gmail.com")
 	m.SetHeader("To", "diaper151@gmail.com")
 	// m.SetAddressHeader("Cc", "dan@example.com", "Dan")
 	m.SetHeader("Subject", "Movie List")
 	// m.SetBody("text/html", "Hello <b>Bob</b> and <i>Cora</i>!")
-	m.Attach("./movie_list.txt")
+	m.Attach(files[0])
 
-	d := mail.NewDialer("smtp.gmail.com", 587, "diaper151@gmail.com", "morris050")
+	d := mail.NewDialer("smtp.gmail.com", 587, "diaper151@gmail.com", "yourpassword")
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 	d.StartTLSPolicy = mail.MandatoryStartTLS
 
@@ -23,4 +29,26 @@ func main() {
 	if err := d.DialAndSend(m); err != nil {
 		panic(err)
 	}
+}
+
+func WalkMatch(root, pattern string) ([]string, error) {
+    var matches []string
+    err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+        if info.IsDir() {
+            return nil
+        }
+        if matched, err := filepath.Match(pattern, filepath.Base(path)); err != nil {
+            return err
+        } else if matched {
+            matches = append(matches, path)
+        }
+        return nil
+    })
+    if err != nil {
+        return nil, err
+    }
+    return matches, nil
 }
