348b576e58eea353c5b051e0e4e29945ec6e5d02
Morris Tai <morristai01@gmail.com>
2020-05-03 16:40:40 +0800

    removed all .txt file before create one.

--- movie_list.ps1
+++ movie_list.ps1
@@ -1,4 +1,5 @@
 echo "Starting Mailing Task"
 cd C:\Users\shanglieh\Documents\movie_list
-tree 'D:\電影' /F > movie_list.txt
+Get-ChildItem $Path | Where{$_.Name -Match ".*.txt"} | Remove-Item 
+tree 'D:\電影' /F > "$(get-date -f yyyy-MM-dd)-movie_list.txt"
 echo "Done!"
--- send_email.go
+++ send_email.go
@@ -1,27 +1,29 @@
 package main
 
 import (
-    "crypto/tls"
-    "fmt"
-    "gopkg.in/mail.v2"
-    "os"
-    "path/filepath"
+	"crypto/tls"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/mail.v2"
 )
 
 func main() {
-	files, err := WalkMatch("/home/morris/Videos", "*.txt")
-        if err != nil{
+	fmt.Println("Start mailing process...")
+	files, err := WalkMatch("C:/Users/shanglieh/Documents/movie_list", "*.txt")
+	if err != nil {
 		panic(err)
 	}
 	m := mail.NewMessage()
-	m.SetHeader("From", "diaper151@gmail.com")
+	m.SetHeader("From", "papatai1112@gmail.com")
 	m.SetHeader("To", "diaper151@gmail.com")
 	// m.SetAddressHeader("Cc", "dan@example.com", "Dan")
 	m.SetHeader("Subject", "Movie List")
 	// m.SetBody("text/html", "Hello <b>Bob</b> and <i>Cora</i>!")
 	m.Attach(files[0])
 
-	d := mail.NewDialer("smtp.gmail.com", 587, "diaper151@gmail.com", "yourpassword")
+	d := mail.NewDialer("smtp.gmail.com", 587, "papatai1112@gmail.com", "yourpassword")
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 	d.StartTLSPolicy = mail.MandatoryStartTLS
 
@@ -29,26 +31,27 @@ func main() {
 	if err := d.DialAndSend(m); err != nil {
 		panic(err)
 	}
+	fmt.Println("Successfully sent list to diaper151@gmail.com")
 }
 
 func WalkMatch(root, pattern string) ([]string, error) {
-    var matches []string
-    err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
-        if info.IsDir() {
-            return nil
-        }
-        if matched, err := filepath.Match(pattern, filepath.Base(path)); err != nil {
-            return err
-        } else if matched {
-            matches = append(matches, path)
-        }
-        return nil
-    })
-    if err != nil {
-        return nil, err
-    }
-    return matches, nil
+	var matches []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if matched, err := filepath.Match(pattern, filepath.Base(path)); err != nil {
+			return err
+		} else if matched {
+			matches = append(matches, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return matches, nil
 }
